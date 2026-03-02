be4b1403b12fe7f3deb5f3cf262808b401b17170
jm-tmp <toobusytocareaboutusername@gmail.com>
2026-03-02 15:01:01 +0530

    Deleted tc1 directory

--- tc1/oled_block.h
+++
@@ -1,8 +0,0 @@
-#ifndef OLED_BLOCK_H
-#define OLED_BLOCK_H
-
-#include <stdint.h>
-
-void OLED_Init_Block(void);
-void OLED_Update_Block(int8_t label_idx);
-#endif
--- tc1/spi_block.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPI_BLOCK_H
-#define SPI_BLOCK_H
-
-void SPI_Send_Temperature_Block(void);
-
-#endif
