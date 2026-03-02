cd355bdf428bc7b028119146930e68d9bbc0d5c6
jm-tmp <toobusytocareaboutusername@gmail.com>
2026-03-02 15:31:08 +0530

    Added header files to application 2

---
+++ temp_class_2/Core/Inc/ai_block.h
@@ -0,0 +1,10 @@
+#ifndef AI_BLOCK_H
+#define AI_BLOCK_H
+
+#include <stdint.h>
+
+void AI_Init_Block(void);
+int8_t AI_Run_Block(void);
+int label_temperature_norm(float temp);
+
+#endif
---
+++ temp_class_2/Core/Inc/dht_block.h
@@ -0,0 +1,9 @@
+#ifndef DHT_BLOCK_H
+#define DHT_BLOCK_H
+
+#include "main.h"
+
+void print_temperature_humidity(void);
+uint8_t DHT_Read_And_Print(void);
+
+#endif
